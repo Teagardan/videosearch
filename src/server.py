bc57d01ac1f0d269792221624b28b6d8c932f796
teagardan <teagardan@outlook.com>
2025-08-06 17:51:00 -0400

    Updated server.py with ngrok Ollama URL

--- src/server.py
+++ src/server.py
@@ -7,6 +7,7 @@ from rank_bm25 import BM25Okapi
 from bisect import bisect_left
 import ollama
 import logging
+os.environ["OLLAMA_HOST"] = "https://xyz789.ngrok-free.app"
 
 app = Flask(__name__, template_folder='templates')
 app.config['BASE_OUTPUT_DIR'] = os.path.expanduser('/Users/siliconweaver/Documents/Teagardan/VIDEO_GEMMA3N/output')
